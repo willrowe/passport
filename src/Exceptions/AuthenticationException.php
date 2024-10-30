e3419c9cd6c6492dd0d8cfa8207fdb0d4761dcc5
Will Rowe <willrowe@users.noreply.github.com>
2024-10-30 14:53:13 -0400

    Include the passport guard when throwing exception

--- src/Exceptions/AuthenticationException.php
+++ src/Exceptions/AuthenticationException.php
@@ -6,4 +6,12 @@ use Illuminate\Auth\AuthenticationException as Exception;
 
 class AuthenticationException extends Exception
 {
+    public function __construct($message = 'Unauthenticated.', array $guards = null, $redirectTo = null)
+    {
+        if (is_null($guards)) {
+            $guards = [config('passport.guard')];
+        }
+
+        parent::__construct($message, $guards, $redirectTo);
+    }
 }
