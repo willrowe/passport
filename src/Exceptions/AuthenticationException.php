a724da9f840e4fa1fac358535475aa9045768dc5
Will Rowe <willrowe@users.noreply.github.com>
2024-10-30 15:02:36 -0400

    Fix config call

--- src/Exceptions/AuthenticationException.php
+++ src/Exceptions/AuthenticationException.php
@@ -3,13 +3,14 @@
 namespace Laravel\Passport\Exceptions;
 
 use Illuminate\Auth\AuthenticationException as Exception;
+use Illuminate\Support\Facades\Config;
 
 class AuthenticationException extends Exception
 {
     public function __construct($message = 'Unauthenticated.', array $guards = null, $redirectTo = null)
     {
         if (is_null($guards)) {
-            $guards = [config('passport.guard')];
+            $guards = [Config::get('passport.guard')];
         }
 
         parent::__construct($message, $guards, $redirectTo);
